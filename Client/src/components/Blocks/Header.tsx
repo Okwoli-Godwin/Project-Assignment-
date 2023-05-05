5a60f525578cdd4864030d95cf18fc8f1b4c1c83
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-05 14:01:12 +0100

    logo added

--- Client/src/components/Blocks/Header.tsx
+++ Client/src/components/Blocks/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import img from "../../assets/img.png"
 
 const Header = () => {
   return (
-      <div className='w-full h-20 flex justify-center z-10 fixed bg-black'>
-          
+      <div className='w-full h-20 flex justify-center z-10 fixed bg-white'>
+          <div className='w-11/12  flex items-center justify-between'>
+              <div className='flex h-screen items-center'>
+                  <img src={img} alt="" className='h-20'/>
+              </div>
+          </div>
     </div>
   )
 }
